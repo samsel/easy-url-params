42b6a9f1eff1c20e078fdbdf78fba87e9d36bb27
Samuel Selvanathan <sjasel@gmail.com>
2013-02-04 21:20:04 -0800

    chrome tab api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 	}, 100);
 	
 	$('button.load').on('click', function() {
-
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+		  console.log(tabs[0].url);
+		});
 	});
 })(jQuery);
