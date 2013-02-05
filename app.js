157383e5b43b644be5100b2d5370297597f59118
Samuel Selvanathan <sjasel@gmail.com>
2013-02-04 21:58:39 -0800

    error markup, basic processing

--- app.js
+++ app.js
@@ -4,15 +4,37 @@
 	}, 100);
 	
 	
+	$('#url').on('keypress', function() {
+		$(".alert").slideUp();
+	});
+		
+		
 	$('button.load').on('click', function() {
 		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-		  urlStringToParamHash(tabs[0].url);
+		  visualizeURLQueryParams(tabs[0].url);
 		});
 	});
 	
 	
-	function urlStringToParamHash(url) {
+	function visualizeURLQueryParams(url) {
 		$('#url').val(url);
 		$('#url').focus();
+		var hash = urlStringToParamHash(url);
 	}
+	
+	function urlStringToParamHash(url) {
+		if ((url == undefined) || ($.trim(url) == "")) {
+			$(".alert").show();
+			$(".alert").html("Please enter a valid URL");
+		}
+		else if (url.indexOf("?") == -1) {
+			$(".alert").show();
+			$(".alert").html("There are no query Params in the URL");			
+		}
+		else {
+			
+		}
+	}
+	
+	
 })(jQuery);
